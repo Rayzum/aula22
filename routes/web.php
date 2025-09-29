09cf6dc8233f5aae100f5a9edf5aea469a3c0927
Rayzum <202151281+Rayzum@users.noreply.github.com>
2025-09-29 07:33:46 -0300

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,19 @@
-<?php
-
 use App\Http\Controllers\TurmaController;
+use App\Http\Controllers\AlunoController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::resource('turmas', TurmaController::class);
+Route::resource('alunos', AlunoController::class);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/sobre', function () {
     return view('sobre');
-    });
-    
+});
 
 Route::get('/formulario', function () {
-return view('formulario');
-    });
-
-   
+    return view('formulario');
+});
 
